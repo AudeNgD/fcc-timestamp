bf070bda0723f4b45e96420f3112a68a24ae4733
AudeNgD <aude.nguyenduc@gmail.com>
2024-05-28 12:35:18 +0100

    complete challenge

--- index.js
+++ index.js
@@ -28,20 +28,43 @@ app.get("/api/hello", function (req, res) {
 //An empty date parameter should return the current time in a JSON object with a unix key and a utc key
 app.get("/api", (req, res) => {
   const date = new Date();
-  const utc_date = moment(date).format("dd, Do MMM YYYY HH:mm:ss GMT");
-  res.json({ unix: Date.now(), utc: utc_date });
+  const utc_date = moment.utc(date).format("ddd, D MMM YYYY HH:mm:ss [GMT]");
+  res.json({ unix: Number(date), utc: utc_date.toString() });
 });
 
 //return the current time in a JSON object with a unix key and a utc key when passed valid date
 app.get("/api/:date", (req, res) => {
-  const date = req.params.date;
-  console.log(date);
-  !isNaN(new Date(date))
-    ? res.json({
-        unix: Date.parse(date),
-        utc: moment(date).format("dd, Do MMM YYYY HH:mm:ss GMT"),
-      })
-    : res.json({ error: "Invalid Date" });
+  let date = req.params.date;
+
+  //case when date is unix timestamp
+  if (date.match(/\d{13}/)) {
+    date = date * 1000;
+    // check if date is valid
+    if (!isNaN(new Date(date))) {
+      return res.json({
+        unix: Number(date / 1000),
+        utc: moment
+          .utc(date / 1000)
+          .format("ddd, DD MMM YYYY HH:mm:ss [GMT]")
+          .toString(),
+      });
+    }
+  }
+
+  // case date is /:date
+  // check if date is valid
+  if (!isNaN(new Date(date))) {
+    const valid_date = Date.parse(date);
+    return res.json({
+      unix: Number(valid_date),
+      utc: moment
+        .utc(valid_date)
+        .format("ddd, DD MMM YYYY HH:mm:ss [GMT]")
+        .toString(),
+    });
+  }
+  // case when date is invalid
+  res.json({ error: "Invalid Date" });
 });
 
 // Listen on port set in environment variable or default to 3000
